0ff654439162ea9f4f86609f139a10d33e94690d
Emmanuel Paniagua <helaman.paniagua4671@alumnos.udg.mx>
2020-10-21 14:17:13 -0500

    cree las varibles para el metodo agregar final, ahora captura e imprime los datos ingresados

--- mainwindow.py
+++ mainwindow.py
@@ -5,15 +5,23 @@ from ui_mainwindow import Ui_MainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        ui = Ui_MainWindow()
-        ui.setupUi(self)
-
-        ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
 
     @Slot()
     def click_agregar(self):
-        print('click') 
+        id = self.ui.id_lineEdit.text()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origen_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
 
+        print (id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
         
              
 
