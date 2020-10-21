8607d2c68dc02e2e4739cf842355272d2842b3fa
Emmanuel Paniagua <helaman.paniagua4671@alumnos.udg.mx>
2020-10-21 15:33:34 -0500

    se añadio el metodo mostrar y agregar inicio

--- mainwindow.py
+++ mainwindow.py
@@ -11,6 +11,12 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar)
+        self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
+        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
+
+    @Slot()
+    def click_mostrar(self):
+        self.administrador.mostrar()
 
     @Slot()
     def click_agregar(self):
@@ -32,4 +38,17 @@ class MainWindow(QMainWindow):
         #self.ui.salida.insertPlainText (id + str(origen_x) + str(origen_y) + str(destino_x) + str(destino_y) + 
         #velocidad + str(red) + str(green) + str(blue))
 
+    @Slot()
+    def click_agregar_inicio(self):
+        id = self.ui.id_lineEdit.text()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origen_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
 
+        particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        self.administrador.agregar_inicio(particula)
